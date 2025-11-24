fe6dbaf38052de95244e1280bb56f8c4eb7d2bc2
Zofia <89477661+SophieBudzik@users.noreply.github.com>
2025-11-24 22:27:20 +0100

    Refactor open_fits_file and clean up code
    Refactor open_fits_file function to accept name directly and handle file types more clearly. Comment out unused code for clarity.
    
    Już widzę potencjalne problemy: w nazwach pliku może być inny filtr  i przydałoby się dodać gdzie te pliki są.

--- basic_fits.py
+++ basic_fits.py
@@ -5,36 +5,36 @@ import matplotlib.pyplot as plt
 from astropy.table import Table
 from astropy.io import fits
 
-def open_fits_file(settings_file, file_type=None):
-    with open(settings_file) as in_file_1:
-        input_ = yaml.safe_load(in_file_1)
-def open_fits_file(settings_file, file_type=None):
-    name = input_["name"]
+def open_fits_file(name, file_type=None):
     if file_type is None:
         f = '_refined_events_ubv_I_Damineli16_'
-    elif file_type is comp:
+    elif file_type == 'comp':
         f = '_refined_events_ubv_I_Damineli16_companions'
-    elif file_type is peaks:
-        f = '_refined_events_ubv_I_Damineli16_companions'
-    elif file_type is lc:
-
-
-
-        f = '_refined_events_ubv_I_Damineli16_lightcurves'
     else:
-        raise ValueError('wrong file type: ' + str(file_type))
+        raise ValueError('wrong file type: ' + str(file_type))  
     fits_table_name = name + f + 'rb.fits'
+    with fits.open(fits_table_name) as hdus:
+        t = Table(hdus[1].data)
+        return t
 
-mask = (t['mass_L'] < 0.15) & (t['isMultiple_L'] == 1.0) & (t['N_companions_S'] == 0.0) & (t['N_companions_L'] == 1.0) & (t['sep_L'] > 0.1)
-t = t[mask]
+if __name__ == '__main__':
+
+    with open(sys.argv[1]) as in_file_1:
+            params = yaml.safe_load(in_file_1)
+
+    field_name = params["name"]
+    data = open_fits_file(field_name)
+
+#mask = (t['mass_L'] < 0.15) & (t['isMultiple_L'] == 1.0) & (t['N_companions_S'] == 0.0) & (t['N_companions_L'] == 1.0) & (t['sep_L'] > 0.1)
+#t = t[mask]
 
 
-comp_list = t['companion_idx_list']
-t_E = t['t_E']
-u_0 = t['u0']
-sep_L = t['sep_L']
+#comp_list = t['companion_idx_list']
+#t_E = t['t_E']
+#u_0 = t['u0']
+#sep_L = t['sep_L']
 
-with open('companions_sep.txt', 'w') as file1:
-    file1.writelines('comp_idx t_E u_0 sep_L \n')
-    for i in range(len(comp_list)):
-        file1.writelines('%s %f %f %f \n' %(comp_list[i], t_E[i], u_0[i], sep_L[i]))
+#with open('companions_sep.txt', 'w') as file1:
+#    file1.writelines('comp_idx t_E u_0 sep_L \n')
+#    for i in range(len(comp_list)):
+#        file1.writelines('%s %f %f %f \n' %(comp_list[i], t_E[i], u_0[i], sep_L[i]))
